8d23ee7ff36c6b67ddf8f2982e743bb1b074f1aa
hyperbotauthor <73212952+hyperbotauthor@users.noreply.github.com>
2021-11-04 00:35:56 +0100

    Repo link

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,21 @@
   <div id="app">
     <img width="40" src="./assets/logo.png" />
     <HelloWorld name="hyperbotauthor" />
+    <div class="source">
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://github.com/hyperbotauthor/vueclisandbox/tree/main/"
+        >View Source Code on GitHub</a
+      >
+      |
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://codesandbox.io/s/musing-edison-1uri7"
+        >Develop in Code Sandbox</a
+      >
+    </div>
     <ChessboardExt> </ChessboardExt>
   </div>
 </template>
@@ -24,5 +39,8 @@ export default {
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   text-align: center;
+}
+.source {
+  margin-bottom: 10px;
 }
 </style>
